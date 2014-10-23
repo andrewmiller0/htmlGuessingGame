0a6df1383775e4d28af13dd7885ebe2b1d31cc23
Andrew Miller <Andrew@Andrews-MacBook-Air.local>
2014-10-22 20:08:45 -0400

    After office hours changes. Most functions working, though need to be refined to meet the final specs

--- js/guess.js
+++ js/guess.js
@@ -3,33 +3,70 @@ var guesses = [];
 var turnCounter = 0;
 var offBy = 50;
 var guess = 101;
+var guessed = false;
+var hint;
 
-$(document).ready(newGame = function(){
-  num = Math.floor(Math.random() * 101);
-  guesses = [];
-  turncounter = 0;
-  console.log("New Game Started. Random number = " + num);
-} );
-
-
-$("#theButton").on( "click", checker = function(){
+var newGame = function(){
   debugger;
-  guess = parseInt($("#guessInput").val());
-  console.log("Guess = " + guess);
-  turnCounter++;
-  if(isNaN(guess) || guess<1 || guess>100){
-    alert("Please Guess a Number between 1-100!");
-  } else {
-    offBy = Math.abs(num-guess);
+    num = Math.floor(Math.random() * 101);
+    guesses = [];
+    turncounter = 0;
+    console.log("New Game Started. Random number = " + num);
   }
-  if(offBy === 0){
-    alert("Winner!");
-  } else if(offBy > 20){
-    alert("You are cold! Guess Again!");
-  } else{
-    alert("You are hot! Guess Again!");
-  }
-  if(turncounter >= 5){
-    alert("Game over!");
-  }
+
+
+$(document).ready(function(){
+
+
+  newGame();
+
+  $("#theButton").on( "click", checker = function(e){
+
+    e.preventDefault();
+    guess = parseInt($("#guessInput").val());
+    console.log("Guess = " + guess);
+    turnCounter++;
+    if(isNaN(guess) || guess<1 || guess>100){
+      alert("Please Guess a Number between 1-100!");
+    } else {
+      offBy = Math.abs(num-guess);
+      for(i=0; i<guesses.length; i++){
+        if(guess === guesses[i]){
+          guessed = true;
+        }
+      }
+      if(guessed){
+        alert("You already guessed that number!");
+        guessed = false;
+      } else if(offBy === 0){
+        alert("Winner!");
+      } else if(offBy > 20){
+        alert("You are cold! Guess Again!");
+      } else{
+        alert("You are hot! Guess Again!");
+      }
+
+    if(turnCounter >= 5){
+      alert("Game over!");
+    }
+    }
+    guesses.push(guess);
+  });
+
+  $("#playAgainButton").on("click", function(){
+    newGame();
+    // newGame();
+  });
+  $("#hintButton").on("click", function(){
+    hint = num-guess;
+    if(turnCounter == 0){
+      alert("You need to submit a guess first!");
+
+    }else if(hint>0){
+      alert("Guess higher!");
+    } else {
+      alert("Guess lower!");
+    }
+  });
+
 });
